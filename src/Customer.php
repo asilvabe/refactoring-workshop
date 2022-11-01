ca12f66920fdb43c562aa0df7fe6e09778db7ba5
Alejandro Silva Berbesí <me@asilvabe.dev>
2022-11-01 16:00:27 -0500

    refactor: rename variables each and thisAmount

--- src/Customer.php
+++ src/Customer.php
@@ -53,36 +53,36 @@ class Customer
         return $result;
     }
 
-    private function amountFor(Rental $each): float
+    private function amountFor(Rental $rental): float
     {
-        $thisAmount = 0;
+        $result = 0;
 
         // determine amounts for each line
-        switch ($each->getMovie()->getPriceCode()) {
+        switch ($rental->getMovie()->getPriceCode()) {
             case Movie::REGULAR:
-                $thisAmount += 2;
+                $result += 2;
 
-                if ($each->getDaysRented() > 2) {
-                    $thisAmount += ($each->getDaysRented() - 2) * 1.5;
+                if ($rental->getDaysRented() > 2) {
+                    $result += ($rental->getDaysRented() - 2) * 1.5;
                 }
 
                 break;
 
             case Movie::NEW_RELEASE:
-                $thisAmount += $each->getDaysRented() * 3;
+                $result += $rental->getDaysRented() * 3;
 
                 break;
 
             case Movie::CHILDRENS:
-                $thisAmount += 1.5;
+                $result += 1.5;
 
-                if ($each->getDaysRented() > 3) {
-                    $thisAmount += ($each->getDaysRented() - 3) * 1.5;
+                if ($rental->getDaysRented() > 3) {
+                    $result += ($rental->getDaysRented() - 3) * 1.5;
                 }
 
                 break;
         }
 
-        return $thisAmount;
+        return $result;
     }
 }
