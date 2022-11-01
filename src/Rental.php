5e6379cba04d8eef3517daedbd57c348285d3d0b
Alejandro Silva Berbesí <me@asilvabe.dev>
2022-11-01 16:26:30 -0500

    refactor: move method getCharge to Movie

--- src/Rental.php
+++ src/Rental.php
@@ -22,35 +22,7 @@ class Rental
     
     public function getCharge(): float
     {
-        $result = 0;
-
-        // determine amounts for each line
-        switch ($this->getMovie()->getPriceCode()) {
-            case Movie::REGULAR:
-                $result += 2;
-
-                if ($this->getDaysRented() > 2) {
-                    $result += ($this->getDaysRented() - 2) * 1.5;
-                }
-
-                break;
-
-            case Movie::NEW_RELEASE:
-                $result += $this->getDaysRented() * 3;
-
-                break;
-
-            case Movie::CHILDRENS:
-                $result += 1.5;
-
-                if ($this->getDaysRented() > 3) {
-                    $result += ($this->getDaysRented() - 3) * 1.5;
-                }
-
-                break;
-        }
-
-        return $result;
+        return $this->getMovie()->getCharge($this->getDaysRented());
     }
 
     public function getFrequentRenterPoints(): int
